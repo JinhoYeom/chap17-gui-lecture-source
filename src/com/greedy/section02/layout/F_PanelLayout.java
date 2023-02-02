119b978f3db2e055081bc79fde571f733e2dedd7
DESKTOP-BFTF0AS\tjoeun <yeomjh2000@gmail.com>
2023-02-02 12:15:11 +0900

    챕터 17 gui

---
+++ src/com/greedy/section02/layout/F_PanelLayout.java
@@ -0,0 +1,15 @@
+package com.greedy.section02.layout;
+
+import javax.swing.JFrame;
+
+public class F_PanelLayout extends JFrame{
+
+	public F_PanelLayout() {
+		super("JPanelLayout");
+		
+		this.setBounds(200, 200, 500, 500);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+}
---
+++ src/com/greedy/section02/layout/run/Application.java
@@ -0,0 +1,16 @@
+package com.greedy.section02.layout.run;
+
+import com.greedy.section02.layout.E_NullLayout;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+//		new A_BorderLayout();
+//		new B_FlowLayout();
+//		new C_GridLayout();
+//		new D_CardLayout();
+		new E_NullLayout();
+		
+	}
+}
